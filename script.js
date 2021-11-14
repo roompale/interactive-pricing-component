f178fd5a031169a04090929b1935be7d12a66f54
roompale <80915644+roompale@users.noreply.github.com>
2021-11-14 20:28:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -33,8 +33,8 @@ let shadow = document.querySelector(".shadow")
 
 
 let page = ["10K PAGEVIEWS", "50K PAGEVIEWS", "100K PAGEVIEWS", "500K PAGEVIEWS", "1M PAGEVIEWS"]
-let price1 = ["$8", "$12", "$16", "$24", "$36"]
-let price2 = ["$6", "$9", "$12", "$18", "$27"]
+let price1 = ["$8.00", "$12.00", "$16.00", "$24.00", "$36.00"]
+let price2 = ["$6.00", "$9.00", "$12.00", "$18.00", "$27.00"]
 let inputnumber = 2
 let outputview = "100K PAGEVIEWS"
 let outputprice = "$16"
